d920443cd5b1dbe8f5f37ee6171c460646466e20
Ashutosh Swarup Srivastava <ashutoshswarup2002@gmail.com>
2025-02-16 22:52:42 +0530

    Restructuring problem solved hopefully

--- js/script.js
+++ js/script.js
@@ -3,18 +3,34 @@ const getButton = document.querySelector('#GetButton');
 
 addButton.addEventListener("click", function(){
     // addButton.innerHTML = "Button Clicked";
+    console.log()
     console.log("Clicked");
 
-    window.location.replace(window.location.origin +
-        "/html/addBookmarkPage.html",
+    if(window.location.protocol === "file:" || window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"){
+      window.location.replace(window.location.origin +
+        "/../html/addBookmarkPage.html",
       );
+    } else {
+      window.location.replace(window.location.origin +
+        "/Bookmarks/html/addBookmarkPage.html",
+      );
+    }
+
+    
 });
 
 getButton.addEventListener('click',function(){
     console.log("Get Bookmarks button Clicked");
 
-    window.location.replace(window.location.origin +
-        "/html/getAllBookmarksPage.html",
+    if(window.location.protocol === "file:" || window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"){
+      window.location.replace(window.location.origin +
+        "/../html/getAllBookmarksPage.html",
       );
+    } else {
+      window.location.replace(window.location.origin +
+        "/Bookmarks/html/getAllBookmarksPage.html",
+      );
+    }
+    
 });
 
